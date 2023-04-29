27f61c45e99f662110a071884bacb4f43445132f
SimaVitalay <simachev28@gmail.com>
2023-04-29 10:33:02 +0300

    first commit

---
+++ src/views/Else.vue
@@ -0,0 +1,15 @@
+<template>
+<div>
+  Some componets
+</div>
+</template>
+
+<script>
+export default {
+  name: "Some"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/Some.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "Some"
+}
+</script>
+
+<style scoped>
+
+</style>
